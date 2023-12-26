209603a6e370a0fab6250e14e6f34d9d8a6c3f39
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-26 10:35:45 +0700

    Create while_break.php

---
+++ while_break.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+  <?php  
+    $i = 0;
+
+    while ($i < 6) {
+      if ($i == 4) break;  
+      echo $i . "<br />";
+      $i++;
+    } 
+  ?>  
+
+</body>
+</html>
