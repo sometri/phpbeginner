36918ea477c15085d300782a7db0789bbe7ef9d2
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-28 17:04:14 +0700

    Create array2.php

---
+++ array2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+  <?php
+    $members = array("Sometri"=>'42', "Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+    foreach ($members as $x => $y) {
+      echo "$x : $y <br>";
+    }
+  ?>
+
+</body>
+</html>
