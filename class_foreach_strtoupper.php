8b80b61d3250f9dfcd7ea9e338c9842565f57184
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-28 17:40:43 +0700

    Create class_foreach_strtoupper.php

---
+++ class_foreach_strtoupper.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>Angkor Wat</h1>
+
+  <?php
+    // Define the Temple class to represent details of Angkor Wat
+    class Temple {
+      public $architect; // variable name
+      public $year;
+      public $origin;
+      
+      // Constructor to initialize temple details
+      public function __construct($architect, $year, $origin) {
+        $this->architect = $architect; // variable name
+        $this->year = $year;
+        $this->origin = $origin;
+      }
+    }
+
+    // Create an instance of the Temple class representing Angkor Wat
+    $myTemple = new Temple("Soriya Varaman II & Jaya Varaman VII", "12th century", "Cambodia");
+
+    // Use foreach to iterate over the properties of the $myTemple object
+    foreach (get_object_vars($myTemple) as $property => $value) {
+      echo strtoupper($property) . ": $value<br>"; // Convert property name to uppercase
+    }
+  ?>
+
+</body>
+</html>
